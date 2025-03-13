b406acbd8cd23d3b8b641e8afc4c9b6e576a4eda
Lena Merkli <lena@merkli.me>
2025-03-13 20:27:36 +0100

    Implement the set user ui and make several smaller improvements

--- create_plusplus_installer.py
+++ create_plusplus_installer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from json import load as json_load, dump as json_dump, JSONDecodeError
-from os import getpid, getlogin
+from os import getlogin
 from os.path import dirname
 from pathlib import Path
 from platform import system as platform_system
@@ -42,12 +42,12 @@ def get_os() -> str:
     return system.replace('Darwin', 'macOS')
 
 
-def get_config(os: str) -> str:
+def get_config() -> str:
     """
     Get the path to the permanent user configuration file
-    :param os: The name of the operating system
     :return: the absolute file path
     """
+    os = get_os()
     if os == 'Linux':
         return f"/home/{getlogin()}/.config/create_plusplus/installer.json"
     elif os == 'macOS':
@@ -58,25 +58,26 @@ def get_config(os: str) -> str:
     return ''
 
 
-def read_config(os: str) -> dict:
+def read_config() -> dict:
     try:
-        with open(get_config(os), 'r') as file:
+        with open(get_config(), 'r') as file:
             contents = json_load(file)
     except (FileNotFoundError, JSONDecodeError):
         contents = DEFAULT_CONFIG
     return contents
 
 
-def write_config(os: str, config: dict) -> None:
-    config_file = get_config(os)
+def write_config(config: dict) -> None:
+    config_file = get_config()
     Path(dirname(config_file)).mkdir(parents=True, exist_ok=True)
-    with open(get_config(os), 'w') as file:
+    with open(config_file, 'w') as file:
         json_dump(config, file)  # type: ignore[arg-type]
     return None
 
 
 
-def get_temp(os: str) -> str:
+def get_temp() -> str:
+    os = get_os()
     if os == 'Linux' or os == 'Darwin':
         return f"/tmp/create_installer/"
     elif os == 'Windows':
@@ -94,7 +95,7 @@ def select(options: list[str]) -> int:
     choice = -1
     while loop:
         try:
-            choice = input("Wählen Sie eine Option: ")
+            choice = input(">>> ")
             if choice == 'c':
                 CONSOLE.print("[orange1 bold]Der Create++ Installer wird beendet.[/]")
                 exit(0)
@@ -142,20 +143,19 @@ def print_table(table: list[list[str]]) -> None:
     return None
 
 
-def detect_specific_user(config: dict) -> str:
+def detect_specific_user() -> str:
     # NOT IMPLEMENTED
-    if 'username' in config and len(config['username']) > 0:
+    config = read_config()
+    if 'username' in config and len(config['username']) >= 3:
         return config['username']
     return 'anonym'
 
 
-def main() -> None:
+def ui_main() -> None:
     os = get_os()
-    config = read_config(os)
-    config_path = get_config(os)
-    temp = get_temp(os)
-    user = detect_specific_user(config)
-    CONSOLE.print(f"[green bold]{HEADING}[/]\n")
+    config_path = get_config()
+    temp = get_temp()
+    user = detect_specific_user()
     print_table([
         ['Installer Version', VERSION],
         ['Betriebssystem', os],
@@ -170,7 +170,38 @@ def main() -> None:
         'Dateien extrahieren',
         'Create++ Installer deinstallieren',
     ])
+    if choice_1 == 1:
+        pass
+    elif choice_1 == 2:
+        pass
+    elif choice_1 == 3:
+        ui_set_user()
+    elif choice_1 == 4:
+        pass
+    elif choice_1 == 5:
+        pass
+    else:
+        raise_error(f"Ungültige Auswahl: {choice_1}")
+    return None
 
+
+def ui_set_user() -> None:
+    CONSOLE.print(f"[purple]{'-' * 12}[/] [purple bold]Bitte geben Sie Ihr Kürzel ein:[/] [purple]{'-' * 12}[/]")
+    CONSOLE.print(f"Oder [purple bold]c[/] um abzubrechen.")
+    user = input(">>> ")
+    if user == 'c':
+        return None
+    config = read_config()
+    config['username'] = user
+    write_config(config)
+    ui_main()
+    return None
+
+
+def main() -> None:
+    CONSOLE.print(f"[green bold]{HEADING}[/]\n")
+    ui_main()
+    return None
 
 
 if __name__ == '__main__':
